3208ac4f673224d8b3c5f4a4676ca60f1fa562e3
Wang KuanChih <n789142536@hotmail.com>
2020-08-11 16:20:30 +0800

    20200811 ver.

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
 
     @IBOutlet weak var restaurantNameLabel: UILabel!
     @IBOutlet weak var restaurantTypeLabel: UILabel!
@@ -19,11 +22,18 @@ class RestaurantDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        restaurantNameLabel.text = restaurant.name
-        restaurantTypeLabel.text = restaurant.type
-        restaurantLocationLabel.text = restaurant.location
-        restaurantImageView.image = UIImage(named: restaurant.image)
+//        restaurantNameLabel.text = restaurant.name
+//        restaurantTypeLabel.text = restaurant.type
+//        restaurantLocationLabel.text = restaurant.location
+//        restaurantImageView.image = UIImage(named: restaurant.image)
         
         navigationItem.largeTitleDisplayMode = .never
+        
+        //設定頭部示圖
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
+        
     }
 }
