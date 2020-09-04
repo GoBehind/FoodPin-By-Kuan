95acb898e4603cab1f4ed0b3d898dfc346a473a8
Wang KuanChih <n789142536@hotmail.com>
2020-09-04 21:59:10 +0800

    20200904 ver. Finish iCloud downloading and uploading. Improved downloading with lazy load

--- FoodPin/Controller/DiscoverTableViewController.swift
+++ FoodPin/Controller/DiscoverTableViewController.swift
@@ -12,6 +12,8 @@ import CloudKit
 class DiscoverTableViewController: UITableViewController {
     
     var restaurants: [CKRecord] = []
+    var spinner = UIActivityIndicatorView()
+    private var imageCache = NSCache<CKRecord.ID, NSURL>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,51 +29,124 @@ class DiscoverTableViewController: UITableViewController {
         }
         
         fetchRecordsFromCloud()
+        
+        spinner.style = .large
+        spinner.hidesWhenStopped = true
+        view.addSubview(spinner)
+        
+        //定義旋轉指示器的layout
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+                                        spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150.0),
+                                        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
+        //啟用spinner
+        spinner.startAnimating()
+        
+        //下拉更新元件
+        refreshControl = UIRefreshControl()
+        refreshControl?.backgroundColor = UIColor.white
+        refreshControl?.tintColor = UIColor.white
+        refreshControl?.addTarget(self, action: #selector(fetchRecordsFromCloud), for: UIControl.Event.valueChanged)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return restaurants.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
+        //設定cell
         let restaurant = restaurants[indexPath.row]
         cell.textLabel?.text = restaurant.object(forKey: "name") as? String
-        if let image = restaurant.object(forKey: "image"), let imageAsset = image as? CKAsset {
-            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) {
+        //設定cell圖片
+        cell.imageView?.image = UIImage(named: "photo")
+        
+        //檢查圖片是否存已儲存在快取中
+        if let imageFileURL = imageCache.object(forKey: restaurant.recordID) {
+            //從快取中取得圖片
+            print("Get image from cache.")
+            if let imageData = try? Data.init(contentsOf: imageFileURL as URL) {
                 cell.imageView?.image = UIImage(data: imageData)
             }
+        } else {
+            //在背景從雲端取得圖片
+            let publicDatabase = CKContainer.default().publicCloudDatabase
+            let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs: [restaurant.recordID])
+            fetchRecordsImageOperation.queuePriority = .veryHigh
+            fetchRecordsImageOperation.desiredKeys = ["image"]
+            
+            fetchRecordsImageOperation.perRecordCompletionBlock = {(record, recordID, error) in
+                if let error = error {
+                    print("Failed to get restaurant image: \(error.localizedDescription)")
+                    return
+                }
+                if let restautantRecord = record,
+                   let image = restautantRecord.object(forKey: "image"),
+                   let imageAsset = image as? CKAsset {
+                    if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) {
+                        //將預設圖片以iCloud圖片取代
+                        DispatchQueue.main.async {
+                            cell.imageView?.image = UIImage(data: imageData)
+                            cell.setNeedsLayout()
+                        }
+                        //加入圖片URL至快取
+                        self.imageCache.setObject(imageAsset.fileURL! as NSURL, forKey: restaurant.recordID)
+                    }
+                }
+            }
+            publicDatabase.add(fetchRecordsImageOperation)
         }
-
         return cell
     }
     
-    func fetchRecordsFromCloud() {
+    @objc func fetchRecordsFromCloud() {
         
-        // Fetch data using Convenience API
+        //更新之前移除目前的資料記錄
+        restaurants.removeAll()
+        tableView.reloadData()
+        
+        // 使用便利型API取得資料
         let cloudContainer = CKContainer.default()
         let publicDatabase = cloudContainer.publicCloudDatabase
         let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: "Restaurant", predicate: predicate)
-        publicDatabase.perform(query, inZoneWith: nil) { (results, error) in
+        //依時間降冪排列
+        query.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+
+        //以query建立查詢動作
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.desiredKeys = ["name"]
+        queryOperation.queuePriority = .veryHigh
+        queryOperation.resultsLimit = 50
+        queryOperation.recordFetchedBlock = {(record) in
+            self.restaurants.append(record)
+        }
+        
+        queryOperation.queryCompletionBlock = {[unowned self](cursor, error) in
             if let error = error {
-                print("Wrong Error", error)
+                print("Failed to get data from iCloud - \(error.localizedDescription)")
                 return
             }
-            if let results = results {
-                print("Completed the download of Restaurant data")
-                self.restaurants = results
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
+            print("Successfully retrieve the data from iCloud")
+            DispatchQueue.main.async {
+                self.spinner.stopAnimating()
+                
+                if let refreshControl = self.refreshControl {
+                    if refreshControl.isRefreshing {
+                        refreshControl.endRefreshing()
+                    }
                 }
+                
+                self.tableView.reloadData()
             }
         }
-
+        //執行查詢
+        publicDatabase.add(queryOperation)
     }
 }
